649486e4514518a5a22d88ae41cdb3fcebbe07b8
AakritiGiri <127657328+AakritiGiri@users.noreply.github.com>
2024-05-20 08:35:06 +0545

    backendFiles

---
+++ back-end/index.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+const Routes = require('./routes/route');
+const Connection = require('./config/database');
+
+const app = express();
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const port = process.env.PORT;
+
+Connection(username, password);
+
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors()); // Allow all origins
+
+app.use('/', Routes);
+
+app.listen(port, () =>
+  console.log(`Server is running successfully on PORT ${port}`)
+);
